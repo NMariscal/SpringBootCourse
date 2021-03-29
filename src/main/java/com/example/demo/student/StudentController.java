f3b6919f31cc4cb58b8d140b8529ab4215ad3143
Natalia Mariscal <n.mariscal.2017@alumnos.urjc.es>
2021-03-29 11:27:40 +0200

    PUT, POST & DELETE implemented

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,10 +1,9 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 // In this class we are going to have all of our resources for the API
@@ -27,4 +26,26 @@ public class StudentController {
         return studentService.getStudents();
     }
 
+    // POST = Add new resources to our system, in this case we are going to add a new student
+    @PostMapping
+    // we take the RequestBody and then we mapping into a Student
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
+
+    //DELETE
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+
+    }
+
+    //PUT: for update the name and the email.
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable ("studentId") Long studentId,
+                              @RequestParam(required=false) String name,
+                              @RequestParam(required=false) String email) {
+        studentService.updatestudent(studentId, name, email);
+
+    }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,7 +2,11 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -16,5 +20,40 @@ public class StudentService {
     // This is going to come of a data base
     public List<Student> getStudents(){
         return studentRepository.findAll(); // This returns a list
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("Email token.");
+        }
+        studentRepository.save(student);
+        System.out.println(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists){
+            throw new IllegalStateException("Student with ID: "+ studentId +" does not exists");
+        }
+        studentRepository.deleteById(studentId);
+
+    }
+
+    @Transactional
+    public void updatestudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException(
+                "Student with Id = " + studentId+" does not exists."));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw  new IllegalStateException("Email taken.");
+            }
+            student.setEmail(email);
+        }
+
     }
 }
